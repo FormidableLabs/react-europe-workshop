cebe94c1d207552f5f6bd4db5de363183f308b27
ebrillhart <emma.brillhart@gmail.com>
2018-05-08 19:19:56 -0700

    start to update structure

--- exercises/portals/solution/src/App.js
+++ exercises/portals/solution/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import './App.css';
-import PortalApp from './portal-app';
 
+import PortalApp from './portal-app';
 
 class App extends Component {
   render() {
