aa95326dac740186ad1c0cb2e80ab0efb103a6b1
ebrillhart <emma.brillhart@gmail.com>
2018-05-08 19:25:14 -0700

    update file structure

--- exercises/form-management/exercise/src/App.js
+++ exercises/form-management/exercise/src/App.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 import FormikForm from './formik-form';
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <FormikForm />
-      </div>
-    );
+    return <FormikForm />
   }
 }
 
