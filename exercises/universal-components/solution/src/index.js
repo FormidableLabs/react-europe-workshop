6f73fa4652f14e30254894911b8a8c1cb32eeae9
Ken Wheeler <thekenwheeler@gmail.com>
2018-05-14 15:49:44 +0200

    universal solution

--- exercises/universal-components/solution/src/index.js
+++ exercises/universal-components/solution/src/index.js
@@ -1,20 +1,80 @@
-import React from 'react';
+import React, { createRef } from 'react';
 import ReactDOM from 'react-dom';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  PanResponder,
+} from 'react-native';
+
+import './index.css';
 
 class App extends React.Component {
+  state = {
+    x: 0,
+    y: 0,
+    background: 'red',
+  };
+  _panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: (evt, gestureState) => true,
+    onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+    onMoveShouldSetPanResponder: (evt, gestureState) => true,
+    onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+    onPanResponderGrant: (evt, gestureState) => {
+      this.setState({
+        background: 'blue',
+      });
+    },
+    onPanResponderMove: (evt, gestureState) => {
+      this.setState(state => ({
+        x: gestureState.dx,
+        y: gestureState.dy,
+      }));
+    },
+    onPanResponderTerminationRequest: (evt, gestureState) => true,
+    onPanResponderRelease: (evt, gestureState) => {
+      this.setState({
+        background: 'red',
+        x: 0,
+        y: 0,
+      });
+    },
+    onPanResponderTerminate: (evt, gestureState) => {},
+    onShouldBlockNativeResponder: (evt, gestureState) => {
+      return true;
+    },
+  });
   render() {
     return (
-      <View style={styles.box}>
-        <Text style={styles.text}>Hello, world!</Text>
+      <View style={styles.container}>
+        <View
+          ref={this.ref}
+          style={[
+            styles.box,
+            {
+              backgroundColor: this.state.background,
+              transform: [
+                { translateX: this.state.x },
+                { translateY: this.state.y },
+              ],
+            },
+          ]}
+          {...this._panResponder.panHandlers}
+        />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  box: { padding: 10 },
-  text: { fontWeight: 'bold' },
+  container: {
+    flex: 1,
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  box: { height: 100, width: 100, backgroundColor: 'red' },
 });
 
 AppRegistry.registerComponent('App', () => App);
